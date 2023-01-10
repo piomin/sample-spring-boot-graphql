adb9c0afa4b4db53ac405c074980773a2b46fc45
piomin <piotr.minkowski@gmail.com>
2023-01-10 16:50:48 +0100

    adding kickstart tests

---
+++ sample-app-kickstart/src/test/java/pl/piomin/samples/spring/graphql/EmployeeQueryResolverTests.java
@@ -0,0 +1,39 @@
+package pl.piomin.samples.spring.graphql;
+
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.piomin.samples.spring.graphql.domain.Employee;
+
+import java.io.IOException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeQueryResolverTests {
+
+    @Autowired
+    GraphQLTestTemplate template;
+
+    @Test
+    void employees() throws IOException {
+        Employee[] employees = template.postForResource("employees.graphql")
+                .get("$.data.employees", Employee[].class);
+        Assertions.assertEquals(7, employees.length);
+    }
+
+    @Test
+    void employeeById() throws IOException {
+        Employee employee = template.postForResource("employeeById.graphql")
+                .get("$.data.employee", Employee.class);
+        Assertions.assertNotNull(employee);
+        Assertions.assertNotNull(employee.getId());
+    }
+
+    @Test
+    void employeesWithFilter() throws IOException {
+        Employee[] employees = template.postForResource("employeesWithFilter.graphql")
+                .get("$.data.employeesWithFilter", Employee[].class);
+        Assertions.assertEquals(4, employees.length);
+    }
+}
